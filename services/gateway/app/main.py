b4bd038bdb41e5d9d3a4fb598afe2a906c905874
Alisson Melo <alisson14melo@gmail.com>
2026-02-06 19:38:17 +0000

    Inicio do projeto SurfPulse - setup base

--- services/gateway/app/main.py
+++ services/gateway/app/main.py
@@ -1,7 +1,26 @@
 from flask import Flask, jsonify
 import requests
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+
 
 app = Flask(__name__)
+
+resource = Resource.create({"service.name": "gateway"})
+provider = TracerProvider(resource=resource)
+trace.set_tracer_provider(provider)
+
+otlp_exporter = OTLPSpanExporter(endpoint="http://jaeger:4318/v1/traces")
+provider.add_span_processor(BatchSpanProcessor(otlp_exporter))
+
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
 
 @app.get("/api")
 def api():
